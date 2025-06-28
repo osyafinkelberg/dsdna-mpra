268803cc7e8f654e265f5e2c9faa2f421129d8b2
Joseph Finkelberg <joseff@bu.edu>
2025-06-28 14:51:43 -0400

    correct virus order in

--- dsdna_mpra/config.py
+++ dsdna_mpra/config.py
@@ -17,28 +17,6 @@ CELL_LINES_COLORS = ['navy', 'cornflowerblue', 'forestgreen', 'deeppink', 'orang
 
 DSDNA_FAMILIES = ['Polyomaviridae', 'Papillomaviridae', 'Adenoviridae', 'Herpesviridae']
 VIRUSES = [
-    # Polyomavirus
-    'Polyomaviridae, BK, Strain MM',
-    'Polyomaviridae, JC, Strain MAD-4',
-    'Polyomaviridae, Merkel Cell Polyoma, Strain MKL-1',
-    # Papillomavirus
-    'Papillomaviridae, Type 1',
-    'Papillomaviridae, Type 2',
-    'Papillomaviridae, Type 5',
-    'Papillomaviridae, Type 6b',
-    'Papillomaviridae, Type 11',
-    'Papillomaviridae, Type 16',
-    'Papillomaviridae, Type 18',
-    'Papillomaviridae, Type 52'
-    # Adenovirus
-    'Adenoviridae, Type 1, Strain Adenoid 71',
-    'Adenoviridae, Type 3, Strain GB',
-    'Adenoviridae, Type 4, Strain RI-67',
-    'Adenoviridae, Type 5, Strain Adenoid 75',
-    'Adenoviridae, Type 7, Strain Gomen',
-    'Adenoviridae, Type 11, Strain Slobitski',
-    'Adenoviridae, Type 14, Strain de Wit',
-    'Adenoviridae, Type 37, Strain GW (76-19026)',
     # Herpesvirus
     'Herpesviridae, Herpes Simplex 1, KOS',
     'Herpesviridae, Herpes Simplex 2, Strain G',
@@ -48,4 +26,26 @@ VIRUSES = [
     'Herpesviridae, 6B',
     'Herpesviridae, Human Herpes 7',
     'Herpesviridae, Kaposi Sarcoma (HHV-8)',
+    # Adenovirus
+    'Adenoviridae, Type 1, Strain Adenoid 71',
+    'Adenoviridae, Type 3, Strain GB',
+    'Adenoviridae, Type 4, Strain RI-67',
+    'Adenoviridae, Type 5, Strain Adenoid 75',
+    'Adenoviridae, Type 7, Strain Gomen',
+    'Adenoviridae, Type 11, Strain Slobitski',
+    'Adenoviridae, Type 14, Strain de Wit',
+    'Adenoviridae, Type 37, Strain GW (76-19026)',
+    # Papillomavirus
+    'Papillomaviridae, Type 1',
+    'Papillomaviridae, Type 2',
+    'Papillomaviridae, Type 5',
+    'Papillomaviridae, Type 6b',
+    'Papillomaviridae, Type 11',
+    'Papillomaviridae, Type 16',
+    'Papillomaviridae, Type 18',
+    'Papillomaviridae, Type 52',
+    # Polyomavirus
+    'Polyomaviridae, BK, Strain MM',
+    'Polyomaviridae, JC, Strain MAD-4',
+    'Polyomaviridae, Merkel Cell Polyoma, Strain MKL-1',
 ]
