2f8800e588e81b8b3d739d61ef65d7d66714fbed
Joseph Finkelberg <joseff@bu.edu>
2025-07-09 08:09:06 -0400

    add genome-wide motif annotation

--- scripts/tf_motif_annotation.py
+++ scripts/tf_motif_annotation.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 import json
 import h5py
@@ -17,18 +18,21 @@ def compute_jaccard_similarity_scores(
 
     num_tiles, num_motifs = contrib_scores.shape[0], len(tf_motifs)
     cjs_arr = np.zeros((num_tiles, num_motifs, 200))
+    with tqdm(total=num_tiles, desc='jaccard similarity', dynamic_ncols=True, leave=False) as pbar:
+        for tile_idx, cs_arr in enumerate(contrib_scores):
+            cjs_arr[tile_idx] = motifs.batch_rolling_cjs(cs_arr, tf_motifs, cwm_shifts)
 
-    for tile_idx, cs_arr in tqdm(
-        enumerate(contrib_scores), total=num_tiles, desc='jaccard similarity'
-    ):
-        cjs_arr[tile_idx] = motifs.batch_rolling_cjs(cs_arr, tf_motifs, cwm_shifts)
+            if (tile_idx + 1) % 10000 == 0 or tile_idx + 1 == num_tiles:
+                step = 10000 if tile_idx + 1 < num_tiles else num_tiles % 10000
+                pbar.update(step)
 
     with h5py.File(
         config.PROCESSED_DIR / 'malinois_K562_contribution_scores_to_motifs_similarity', "w"
     ) as f:
-        dset = f.create_dataset("continuous_jaccard_similarity", data=cjs_arr, dtype='f')
-        dset.attrs["tile_ids"] = tile_ids.astype('S')
-        dset.attrs["motif_ids"] = np.array(sorted(tf_motifs.keys()), dtype='S')
+        _ = f.create_dataset("continuous_jaccard_similarity", data=cjs_arr, dtype='f')
+        dt = h5py.string_dtype(encoding='utf-8')
+        f.create_dataset("tile_ids", data=tile_ids.astype(dt))
+        f.create_dataset("motif_ids", data=np.array(sorted(tf_motifs.keys()), dtype=dt))
 
 
 def match_motifs_to_contribution_score_peaks(
@@ -37,71 +41,149 @@ def match_motifs_to_contribution_score_peaks(
     contrib_scores: np.ndarray,
     tile_ids: np.ndarray
 ) -> None:
-    tacs_window = 5
-    per_pos_threshold = 0.15
-    peak_radius = 5
-    overlap_threshold = 0.2
 
     cs_peaks = []
-    for tile_idx in tqdm(range(contrib_scores.shape[0]), desc='tiles peak calling'):
-        cs_arr = contrib_scores[tile_idx]
-        tacs = motifs.rolling_absolute_contribution_scores(cs_arr, window=tacs_window)
-        peaks = motifs.call_absolute_contribution_peaks(
-            tacs=tacs,
-            summit_threshold=per_pos_threshold * tacs_window,
-            peak_radius=peak_radius,
-            overlap_threshold=overlap_threshold
-        )
-        matched_tacs = tacs[peaks.mean(1).astype(int)]
-        cs_peaks.append((peaks, matched_tacs))
+    num_tiles = contrib_scores.shape[0]
+    with tqdm(total=num_tiles, desc='tiles peak calling', dynamic_ncols=True, leave=False) as pbar:
+        for tile_idx in range(num_tiles):
+            cs_arr = contrib_scores[tile_idx]
+            tacs = motifs.rolling_absolute_contribution_scores(cs_arr, window=config.TACS_WINDOW)
+            peaks = motifs.call_absolute_contribution_peaks(
+                tacs=tacs,
+                summit_threshold=config.PER_POS_THRESHOLD * config.TACS_WINDOW,
+                peak_radius=config.PEAK_RADIUS,
+                overlap_threshold=config.OVERLAP_THRESHOLD
+            )
+            matched_tacs = tacs[peaks.mean(1).astype(int)]
+            cs_peaks.append((peaks, matched_tacs))
+
+            if (tile_idx + 1) % 10000 == 0 or tile_idx + 1 == num_tiles:
+                step = 10000 if tile_idx + 1 < num_tiles else num_tiles % 10000
+                pbar.update(step)
 
     cjs_path = config.PROCESSED_DIR / 'malinois_K562_contribution_scores_to_motifs_similarity'
     with h5py.File(cjs_path, "r") as f:
         cjs_arr = f["continuous_jaccard_similarity"][()]
-        tile_ids = f["continuous_jaccard_similarity"].attrs["tile_ids"].astype(str)
-        motif_ids = f["continuous_jaccard_similarity"].attrs["motif_ids"].astype(str)
+        tile_ids = f["tile_ids"][()].astype(str)
+        motif_ids = f["motif_ids"][()].astype(str)
 
     cwm_shifts = np.array([cwm_shifts_unordered[motif_id] for motif_id in motif_ids])
 
-    best_match = {}
-    for tile_idx, (peak_positions, match_tacs) in tqdm(
-        enumerate(cs_peaks), total=len(cs_peaks), desc='map top-1 CJS match'
-    ):
-        if not peak_positions.size:
-            best_match[tile_idx] = {
+    best_match = []
+    with tqdm(total=num_tiles, desc='map top-1 CJS match', dynamic_ncols=True, leave=False) as pbar:
+        for tile_idx, (peak_positions, match_tacs) in enumerate(cs_peaks):
+            if not peak_positions.size:
+                best_match.append({
+                    "tile_id": tile_ids[tile_idx],
+                    "peak_positions": [],
+                    "motif_positions": [],
+                    "motifs": [],
+                    "tacs": [],
+                    "cjs": []
+                })
+                continue
+
+            match_pos, match_cjs, match_motif = motifs.find_best_motif_matches(
+                cjs_arr[tile_idx],
+                motif_ids,
+                cwm_shifts,
+                peak_positions.mean(1).astype(np.uint8),
+                config.PEAK_RADIUS,
+                top_k=1
+            )
+
+            match_positions = [
+                [pos, pos + tf_motifs[motif].shape[1]]
+                for pos, motif in zip(match_pos.squeeze(1).tolist(), match_motif.squeeze(1).tolist())
+            ]
+
+            best_match.append({
                 "tile_id": tile_ids[tile_idx],
-                "positions": [],
-                "motifs": [],
-                "tacs": [],
-                "cjs": []
-            }
-            continue
+                "peak_positions": peak_positions.astype(int).tolist(),
+                "motif_positions": match_positions,
+                "motifs": match_motif.squeeze(1).tolist(),
+                "tacs": match_tacs.tolist(),
+                "cjs": match_cjs.squeeze(1).tolist()
+            })
 
-        match_pos, match_cjs, match_motif = motifs.find_best_motif_matches(
-            cjs_arr[tile_idx],
-            motif_ids,
-            cwm_shifts,
-            peak_positions.mean(1).astype(np.uint8),
-            peak_radius,
-            top_k=1
-        )
-
-        match_positions = [
-            [pos, pos + tf_motifs[motif].shape[1]]
-            for pos, motif in zip(match_pos.squeeze(1).tolist(), match_motif.squeeze(1).tolist())
-        ]
-
-        best_match[tile_idx] = {
-            "tile_id": tile_ids[tile_idx],
-            "positions": match_positions,
-            "motifs": match_motif.squeeze(1).tolist(),
-            "tacs": match_tacs.tolist(),
-            "cjs": match_cjs.squeeze(1).tolist()
-        }
+            if (tile_idx + 1) % 10000 == 0 or tile_idx + 1 == num_tiles:
+                step = 10000 if tile_idx + 1 < num_tiles else num_tiles % 10000
+                pbar.update(step)
 
     save_path = config.RESULTS_DIR / "malinois_K562_tf_motif_map.json"
     with open(save_path, 'w', encoding='utf-8') as f:
         json.dump(best_match, f, ensure_ascii=False, indent=1)
+
+
+def build_genome_wide_motif_instances_map() -> None:
+    # load list of virus genomes
+    virus_genomes = pd.read_csv(config.RAW_DIR / 'virus_genbank_ids.txt').columns.values
+    # load and filter paired tiles
+    paired_tiles = pd.read_csv(config.PROCESSED_DIR / 'virus_paired_tiles_log2p_ratios.csv')
+    paired_tiles = paired_tiles[
+        paired_tiles['genome'].isin(virus_genomes) &
+        paired_tiles['family'].isin(config.DSDNA_FAMILIES)
+    ]
+    paired_tiles = (
+        paired_tiles
+        .drop_duplicates(['genome', 'begin'])[
+            ['family', 'strain', 'tile_id', 'genome', 'begin']
+        ]
+        .sort_values(['genome', 'begin'])
+        .reset_index(drop=True)
+    )
+    # load tile-to-motif mapping
+    with open(config.RESULTS_DIR / "malinois_K562_tf_motif_map.json", 'r', encoding='utf-8') as f:
+        tile_motif_map = {
+            tile_map['tile_id']: tile_map
+            for tile_map in json.load(f)
+        }
+    # load motif-to-gene mapping
+    motif_to_gene_map = dict(pd.read_csv(config.RESULTS_DIR / 'malinois_K562_tf_motif_families.csv').values)
+
+    # build genome-wide motif annotation
+    def pick_max_tacs_match(group: pd.DataFrame, include_groups: bool = False) -> pd.Series:
+        """
+        Selects the row with the highest contribution score from overlapping motif instances.
+        """
+        return group.loc[group['contribution_score'].idxmax()]
+
+    all_genome_maps = []
+    for genome_id, genome_tiles in paired_tiles.groupby('genome'):
+        motif_annotations = []
+        for family, strain, tile_id, genome, tile_start in genome_tiles.itertuples(index=False):
+            tile_data = tile_motif_map[tile_id]
+            motifs = tile_data['motifs']
+            positions = tile_data['motif_positions']
+            tacs_scores = tile_data['tacs']
+            cj_scores = tile_data['cjs']
+            for motif, (start, end), tacs, cjs in zip(motifs, positions, tacs_scores, cj_scores):
+                motif_clean = motif.removesuffix('_fwd').removesuffix('_rev')
+                gene = motif_to_gene_map.get(motif_clean, 'Unknown')
+                motif_annotations.append({
+                    'family': family,
+                    'strain': strain,
+                    'tile_id': tile_id,
+                    'genome': genome,
+                    'tile_begin': tile_start,
+                    'tf_motif': motif,
+                    'tf_gene': gene,
+                    'begin': tile_start + start,
+                    'end': tile_start + end,
+                    'contribution_score': tacs,
+                    'continuous_jaccard_similarity': cjs
+                })
+        motif_df = pd.DataFrame(motif_annotations)
+        # deduplicate overlapping motif instances using a unique identifier
+        motif_df['instance_id'] = motif_df[['tf_motif', 'begin', 'end']].astype(str).agg('-'.join, axis=1)
+        motif_df = motif_df.groupby('instance_id').apply(pick_max_tacs_match, include_groups=False)
+        motif_df.sort_values(['begin', 'end', 'tile_begin'], inplace=True)
+        all_genome_maps.append(motif_df.reset_index(drop=True))
+
+    # concatenate all genome-level maps and save
+    final_df = pd.concat(all_genome_maps).reset_index(drop=True)
+    output_path = config.RESULTS_DIR / 'malinois_K562_tf_motif_genome_annotation.csv'
+    final_df.to_csv(output_path, index=False)
 
 
 if __name__ == "__main__":
@@ -123,3 +205,4 @@ if __name__ == "__main__":
 
     compute_jaccard_similarity_scores(tf_motifs, cwm_shifts, contrib_scores, tile_ids)
     match_motifs_to_contribution_score_peaks(tf_motifs, cwm_shifts, contrib_scores, tile_ids)
+    build_genome_wide_motif_instances_map()
