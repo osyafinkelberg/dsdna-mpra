6697e1631dbb573d7c33d0a39526cc2c95df20fe
Joseph Finkelberg <joseff@bu.edu>
2025-07-19 11:06:31 -0400

    replace motif annotation functions from script to dsdna_mpra module

--- dsdna_mpra/motifs.py
+++ dsdna_mpra/motifs.py
@@ -5,6 +5,12 @@ import numpy as np
 import pandas as pd
 from numpy.lib.stride_tricks import sliding_window_view
 
+from tqdm import tqdm
+import json
+import h5py
+
+from . import config
+
 
 """
 PWM Processing
@@ -330,3 +336,115 @@ def build_contribution_score_dataframes(
             cs_array[:, pos: pos + cwm.shape[1]] = cwm
         cs_dfs.append(pd.DataFrame(cs_array.T, columns=['A', 'C', 'G', 'T']))
     return tuple(cs_dfs)
+
+
+"""
+Utility functions for large-scale analysis.
+"""
+
+
+def compute_jaccard_similarity_scores(
+    tf_motifs: dict[str, np.ndarray],
+    cwm_shifts: dict[str, int],
+    contrib_scores: np.ndarray,
+    tile_ids: np.ndarray,
+    output_path: Path
+) -> None:
+
+    num_tiles, num_motifs = contrib_scores.shape[0], len(tf_motifs)
+    cjs_arr = np.zeros((num_tiles, num_motifs, 200))
+    with tqdm(total=num_tiles, desc='jaccard similarity', dynamic_ncols=True, leave=False) as pbar:
+        for tile_idx, cs_arr in enumerate(contrib_scores):
+            cjs_arr[tile_idx] = batch_rolling_cjs(cs_arr, tf_motifs, cwm_shifts)
+
+            if (tile_idx + 1) % 10000 == 0 or tile_idx + 1 == num_tiles:
+                step = 10000 if tile_idx + 1 < num_tiles else num_tiles % 10000
+                pbar.update(step)
+
+    with h5py.File(
+        output_path, "w"
+    ) as f:
+        _ = f.create_dataset("continuous_jaccard_similarity", data=cjs_arr, dtype='f')
+        dt = h5py.string_dtype(encoding='utf-8')
+        f.create_dataset("tile_ids", data=tile_ids.astype(dt))
+        f.create_dataset("motif_ids", data=np.array(sorted(tf_motifs.keys()), dtype=dt))
+
+
+def match_motifs_to_contribution_score_peaks(
+    tf_motifs: dict[str, np.ndarray],
+    cwm_shifts_unordered: dict[str, int],
+    contrib_scores: np.ndarray,
+    tile_ids: np.ndarray,
+    output_path: Path
+) -> None:
+
+    cs_peaks = []
+    num_tiles = contrib_scores.shape[0]
+    with tqdm(total=num_tiles, desc='tiles peak calling', dynamic_ncols=True, leave=False) as pbar:
+        for tile_idx in range(num_tiles):
+            cs_arr = contrib_scores[tile_idx]
+            tacs = rolling_absolute_contribution_scores(cs_arr, window=config.TACS_WINDOW)
+            peaks = call_absolute_contribution_peaks(
+                tacs=tacs,
+                summit_threshold=config.PER_POS_THRESHOLD * config.TACS_WINDOW,
+                peak_radius=config.PEAK_RADIUS,
+                overlap_threshold=config.OVERLAP_THRESHOLD
+            )
+            matched_tacs = tacs[peaks.mean(1).astype(int)]
+            cs_peaks.append((peaks, matched_tacs))
+
+            if (tile_idx + 1) % 10000 == 0 or tile_idx + 1 == num_tiles:
+                step = 10000 if tile_idx + 1 < num_tiles else num_tiles % 10000
+                pbar.update(step)
+
+    cjs_path = config.PROCESSED_DIR / 'malinois_K562_contribution_scores_to_motifs_similarity'
+    with h5py.File(cjs_path, "r") as f:
+        cjs_arr = f["continuous_jaccard_similarity"][()]
+        tile_ids = f["tile_ids"][()].astype(str)
+        motif_ids = f["motif_ids"][()].astype(str)
+
+    cwm_shifts = np.array([cwm_shifts_unordered[motif_id] for motif_id in motif_ids])
+
+    best_match = []
+    with tqdm(total=num_tiles, desc='map top-1 CJS match', dynamic_ncols=True, leave=False) as pbar:
+        for tile_idx, (peak_positions, match_tacs) in enumerate(cs_peaks):
+            if not peak_positions.size:
+                best_match.append({
+                    "tile_id": tile_ids[tile_idx],
+                    "peak_positions": [],
+                    "motif_positions": [],
+                    "motifs": [],
+                    "tacs": [],
+                    "cjs": []
+                })
+                continue
+
+            match_pos, match_cjs, match_motif = find_best_motif_matches(
+                cjs_arr[tile_idx],
+                motif_ids,
+                cwm_shifts,
+                peak_positions.mean(1).astype(np.uint8),
+                config.PEAK_RADIUS,
+                top_k=1
+            )
+
+            match_positions = [
+                [pos, pos + tf_motifs[motif].shape[1]]
+                for pos, motif in zip(match_pos.squeeze(1).tolist(), match_motif.squeeze(1).tolist())
+            ]
+
+            best_match.append({
+                "tile_id": tile_ids[tile_idx],
+                "peak_positions": peak_positions.astype(int).tolist(),
+                "motif_positions": match_positions,
+                "motifs": match_motif.squeeze(1).tolist(),
+                "tacs": match_tacs.tolist(),
+                "cjs": match_cjs.squeeze(1).tolist()
+            })
+
+            if (tile_idx + 1) % 10000 == 0 or tile_idx + 1 == num_tiles:
+                step = 10000 if tile_idx + 1 < num_tiles else num_tiles % 10000
+                pbar.update(step)
+
+    with open(output_path, 'w', encoding='utf-8') as f:
+        json.dump(best_match, f, ensure_ascii=False, indent=1)
